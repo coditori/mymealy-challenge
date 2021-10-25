9f31c1848ad38bebabd61e8c933942453520ce2c
behrooz mohamadi <behrooz.mohamadi.66@gmail.com>
2021-10-25 18:10:37 +0330

    add MockApiTest change some unnecessary annotation from header class

--- src/test/java/com/mymealy/challenge/MockApiTest.java
+++ src/test/java/com/mymealy/challenge/MockApiTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,17 +23,24 @@ import java.math.BigDecimal;
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
-public class ApiTest {
+public class MockApiTest {
     @Value("${binance.api.url}")
     private String binanceApiUrl;
     @Value("${binance.api.symbol}")
     private String binanceApiSymbol;
-    @Autowired
+    @Mock
     private RestTemplate restTemplate;
+
+    public void setup(){
+        Mockito.when(restTemplate.getForEntity(binanceApiUrl.concat("/api/v3/ping"), String.class)).thenReturn(new ResponseEntity("", HttpStatus.OK));
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(binanceApiUrl.concat("/api/v3/avgPrice")).queryParam("symbol", binanceApiSymbol);
+        Mockito.when(restTemplate.getForEntity(uriComponentsBuilder.toUriString(), RateHistoryEntity.class)).thenReturn(new ResponseEntity(new RateHistoryEntity(){{setPrice(new BigDecimal(60000));}}, HttpStatus.OK));
+    }
 
     @Test
     @Order(1)
-    public void ping(){
+    public void ping() {
+        setup();
         ResponseEntity<String> response = restTemplate.getForEntity(binanceApiUrl.concat("/api/v3/ping"), String.class);
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
@@ -40,9 +48,10 @@ public class ApiTest {
     @Test
     @Order(2)
     public void priceAvg(){
+        setup();
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(binanceApiUrl.concat("/api/v3/avgPrice")).queryParam("symbol", binanceApiSymbol);
         ResponseEntity<RateHistoryEntity> responseEntity = restTemplate.getForEntity(uriComponentsBuilder.toUriString(), RateHistoryEntity.class);
         Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(responseEntity.getBody().getPrice()).isGreaterThan(BigDecimal.ZERO);
+        Assertions.assertThat(responseEntity.getBody().getPrice()).isEqualTo(new BigDecimal(60000));
     }
 }
