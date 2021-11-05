2e6a2b3b161c9f185956e64119bdb1a1a7a47fbf
mkareshky <69905577+mkareshky@users.noreply.github.com>
2021-11-05 18:23:50 +0330

    live list

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CryptoList from '../components/CryptoList';
 import SearchBar from '../components/SearchBar';
 import Layout from '../components/Layout';
 
+export const fetchData = async () => {
+  const res = await fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+  );
+  return await res.json();
+};
+
 export default function Home({ filteredCryptos }) {
   const [search, setSearch] = useState('');
+  const [prices, setPrices] = useState(filteredCryptos.sort(function (a, b) {
+    return b.current_price - a.current_price;
+  }));
 
-  const allCryptos = filteredCryptos.filter(crypto =>
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      //This will run every second!
+      const result = await fetchData();
+      setPrices(result);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const allCryptos = prices.filter(crypto =>
     crypto.name.toLowerCase().includes(search.toLowerCase())
   );
 
-  allCryptos.sort(function (a, b) {
-    return b.current_price - a.current_price;
-  });
-
   const handleChange = e => {
     e.preventDefault();
-
     setSearch(e.target.value.toLowerCase());
   };
 
@@ -31,11 +45,8 @@ export default function Home({ filteredCryptos }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await fetch(
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
-  );
 
-  const filteredCryptos = await res.json();
+  const filteredCryptos = await fetchData();
 
   return {
     props: {
